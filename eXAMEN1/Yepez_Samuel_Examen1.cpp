3ffa47963fef7dc56a6f27559a6f5bbb60e96049
SamuelYH <samixs146@gmail.com>
2026-09-10 15:23:20 -0500

    version final del examen1

--- eXAMEN1/Yepez_Samuel_Examen1.cpp
+++ eXAMEN1/Yepez_Samuel_Examen1.cpp
@@ -15,11 +15,15 @@ int main(){
     int estudiantes;
     int Aprobado, Reprobado;
     int posicion = -1;
-    int buscando;
-    int encontado = false;
+    string buscando;
+    bool encontado = false;
     double promedio = 0.0;
     double suma = 0.0;
-    
+    int aprobados = 0;
+    int reprobados = 0;
+    int notaMayor, notaMenor;
+    string estudianteMayor, estudianteMenor;
+
     
     
     do
@@ -31,7 +35,7 @@ int main(){
       cin >> opcion;  
         switch (opcion)
         {
-        case 1:
+            case 1:
             cout <<"Ingrese el numero de estudiantes: ";  
             cin >> estudiantes;     
             while (estudiantes < 0 or estudiantes > 20)
@@ -40,19 +44,19 @@ int main(){
                 cin >> estudiantes;
             }
             
-            cout <<"Ingrese el Primer nombre del estudiante: " << endl;   
+            cout <<"Ingrese el Primer nombre del estudiante " << endl;   
                    
 
             
             for ( i = 1; i <= estudiantes;i++){
-            cout <<"Nombre: " << endl;     
+            cout <<"Nombre: ";     
             
             cin >> nombres[i];}
-            cout <<"Ingrese la nota del estudiante: " << endl;
+            cout <<"Ingrese la nota del estudiante " << endl;
             
             
             for ( i = 1; i <= estudiantes; i++){
-                cout <<"Nota: " << endl;
+                cout <<"Nota: ";
                 cin >> notas[i];
                  
                  while (notas[i] < 0 or notas[i] > 20){
@@ -61,9 +65,13 @@ int main(){
             
             
             break;
-        case 2:
+            case 2:
            
             cout <<"Nombres - Notas - Estado " << endl;
+            notaMayor = notas[1];
+            estudianteMayor = nombres[1]; 
+            notaMenor = notas[1];
+            estudianteMenor = nombres[1];
             for ( i = 1; i <= estudiantes; i++)
             {
                 cout << nombres[i] << "-" << notas[i] << "-"; 
@@ -71,28 +79,80 @@ int main(){
              if (notas[i] >= 14 and notas[i] <= 20)
             {
                 cout <<"Aprobado" << endl;
+                aprobados ++;
             }else{
                 cout <<"Reprobado" << endl;
+                reprobados ++;
             }  
-        suma = suma + notas[i];} 
+            suma = suma + notas[i];
+            if (notas[i] > notaMayor )
+            {
+                notaMayor = notas[i];
+                estudianteMayor = nombres[i];
+            }
+            if (notas[i] < notaMenor)
+            {
+                notaMenor = notas[i];
+                estudianteMenor = nombres[i];
+            }
+            
+            } 
             
             if(estudiantes > 0)
             {
                 promedio = suma/ estudiantes;
             cout <<"Promedio general: " << promedio << endl;
            
-            }break;
+            }
+             cout <<"El estudinate con la nota mas alta es: " << estudianteMayor << " con " << notaMayor << endl;
+             cout <<"El estudinate con la nota mas baja es: " << estudianteMenor << " con " << notaMenor << endl;
+             cout <<"Numero de aprobados: " << aprobados << endl;
+             cout <<"Numero de reprobados: "<< reprobados << endl;  
+            
+            break;
    
-        case 3:    
-        case 4: 
+            case 3:
+            cout <<"Ingrese el nombre del estudinate que desea buscar: ";
+            cin >> buscando;
+            encontado = false;
+            for ( i = 1; i < estudiantes; i++)
+            {
+              if (nombres[i] == buscando and encontado == false  )
+                {
+                    encontado = true;
+                    posicion = i;
+                }
+            if (encontado == true)
+            {
+                cout << nombres[i] << " - " << notas[i] << " - "; 
+           
+            if (notas[i] >= 14 and notas[i] <= 20)
+            {
+                cout <<"Aprobado" << endl;
+                
+            }else{
+                cout <<"Reprobado" << endl;
+                
+            }
+            }
+  
+            }if (encontado == false)
+            {
+                cout <<"El estudiante " << buscando << " no se encuentra registrado " << endl;
+            }
+            break;
+                
+            case 4: 
               cout <<"Saliendo...";
+              break;
                         
             
                  
         
-        default:
+            default:
+            cout <<"Opcion invalida, intente de nuevo."<< endl;
             break;
         }
     } while (opcion != 4);
-    
+    return 0;
 }
