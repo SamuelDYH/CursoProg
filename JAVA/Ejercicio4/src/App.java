6c87f6dabdcfafdd9cf1e70e9e06da23834fe238
SamuelYH <samixs146@gmail.com>
2026-09-18 14:32:53 -0500

    creacion de ejercio para practicar el tema de constructores

--- JAVA/Ejercicio4/src/App.java
+++ JAVA/Ejercicio4/src/App.java
@@ -7,5 +7,26 @@ ENUNCIADO
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Creamos el primer objeto usando el constructor
+        Producto producto1 = new Producto("Mouse", 15.5, 10);
+        
+        // Creamos el segundo onjeto con el constructor
+        Producto producto2 = new Producto("Teclado", 25.00, 5);
+        
+        System.out.println("====PRODUCTO 1====");
+        // Llamamos al metodo mostrarinfo del primer onjeto
+        producto1.mostrarinfo();
+
+        System.out.println("Valor total en el inventario: " + producto1.calcularValorInventario());
+
+        System.out.println();
+
+        System.out.println("====PRODUCTO 2====");
+        // Llamamos a la funciom
+        producto2.mostrarinfo();
+
+        System.out.println("Valor total en el inventario: " + producto2.calcularValorInventario());
     }
+
+
 }
